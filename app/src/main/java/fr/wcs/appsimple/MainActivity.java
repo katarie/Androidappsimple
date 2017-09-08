bce0c107291f603fb58801988e0477f272b468ba
katarie <valentin.veler2@gmail.com>
2017-09-08 10:48:24 +0200

    clic checkbox

--- app/src/main/java/fr/wcs/appsimple/MainActivity.java
+++ app/src/main/java/fr/wcs/appsimple/MainActivity.java
@@ -2,6 +2,10 @@ package fr.wcs.appsimple;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CheckBox checkbox = (CheckBox) findViewById(R.id.checkbox);
+        checkbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editfirstname = (EditText) findViewById(R.id.firstname);
+                EditText editlastname = (EditText) findViewById(R.id.lastname);
+                Button button = (Button) findViewById(R.id.button);
+                if (checkbox.isChecked()) {
+
+                    editfirstname.setEnabled(true);
+                    editlastname.setEnabled(true);
+                    button.setEnabled(true);
+
+                } else {
+
+                    editfirstname.setEnabled(false);
+                    editlastname.setEnabled(false);
+                    button.setEnabled(false);
+
+                }
+
+            }
+        });
     }
 }
