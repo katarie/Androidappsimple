5b5133728aeebe68a0bfe9b4175e59d0c6b8ba47
katarie <valentin.veler2@gmail.com>
2017-09-08 11:39:10 +0200

    ma quete fonctionne

--- app/src/main/java/fr/wcs/appsimple/MainActivity.java
+++ app/src/main/java/fr/wcs/appsimple/MainActivity.java
@@ -1,11 +1,14 @@
 package fr.wcs.appsimple;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,5 +40,37 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText editfirstname = (EditText) findViewById(R.id.firstname);
+                EditText editlastname = (EditText) findViewById(R.id.lastname);
+                if (editfirstname.getText().toString().isEmpty()
+                        || editlastname.getText().toString().isEmpty()){
+
+                    Context context = getApplicationContext();
+                    CharSequence text = getResources().getString(R.string.refut);
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+                }
+                else {
+
+                    TextView textCongratulations = (TextView) findViewById(R.id.Congratulations);
+                    textCongratulations.setText(getResources().getString(R.string.Congratulations)
+
+                            +" " + editfirstname.getText().toString()
+                                +" " + editlastname.getText().toString()
+                    );
+
+                }
+            }
+        });
+
     }
 }
